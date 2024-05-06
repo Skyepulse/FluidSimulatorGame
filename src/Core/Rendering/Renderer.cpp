a7df02045175906c1e52582f304c292e055066d8
arthur <arthur.wuhrlin@gmail.com>
2024-05-06 15:44:22 +0200

    one circle

--- src/Core/Rendering/Renderer.cpp
+++ src/Core/Rendering/Renderer.cpp
@@ -20,7 +20,7 @@ void Renderer::Draw(const Transform& transform, std::shared_ptr<Shader> shader, 
   glDrawElements(GL_TRIANGLES, va.GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
 }
 
-void Renderer::Draw(const Transform &transform, std::shared_ptr<Shader> shader, const Texture& texture, const VertexArray &va)
+void Renderer::Draw(const Transform &transform, std::shared_ptr<Shader> shader, const Texture& texture, const VertexArray& va)
 {
   va.Bind();
   shader->Bind();
@@ -34,4 +34,22 @@ void Renderer::Draw(const Transform &transform, std::shared_ptr<Shader> shader, 
 
   glDrawElements(GL_TRIANGLES, va.GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
   texture.Unbind();
+}
+
+void Renderer::DrawPoint(const Transform &transform, std::shared_ptr<Shader> shader, const VertexArray& va)
+{
+  va.Bind();
+  shader->Bind();
+
+  shader->SetMat4("u_VPMatrix", m_VPMatrix);
+  shader->SetMat4("u_ModelMatrix", transform.GetModelMatrix());
+
+  glEnable(GL_SMOOTH_POINT_SIZE_RANGE);
+  glPointSize(50);
+  glEnable( GL_BLEND );
+  glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+  glDrawElements(GL_POINTS, va.GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+
+  shader->Unbind();
+  va.Unbind();
 }
