cc35c8aa79bbee185ea6ab6ab042c9e2c036bfa8
arthur <arthur.wuhrlin@gmail.com>
2024-05-06 15:54:29 +0200

    Basic renderer commands

---
+++ src/Core/Rendering/RendererCommand.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <glad/glad.h>
+
+#include <glm/glm.hpp>
+
+class RendererCommand
+{
+public:
+  static void Clear() { glClear(GL_COLOR_BUFFER_BIT); }
+  
+  static void ClearColor(float r = 0.3f, float g = 0.4f, float b = 0.4f, float a = 1.0f) { glClearColor(r, g, b, a); }
+
+  static void EnableBlending(bool enabled) { enabled ? glEnable(GL_BLEND) : glDisable(GL_BLEND); }
+};
