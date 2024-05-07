e970b75d62f1f7138faff1b7fa8275afbf73e05c
arthur <arthur.wuhrlin@gmail.com>
2024-05-07 19:45:51 +0200

    Stored Camera origin and size

--- src/Core/Rendering/Camera.h
+++ src/Core/Rendering/Camera.h
@@ -14,7 +14,14 @@ public:
 
   Transform2D& GetTransform2D() { return m_Transform2D; }
   Transform& GetTransform() { return dynamic_cast<Transform&>(m_Transform2D); }
+
+  const glm::vec2& GetSize() const { return m_Size; }
+  float GetWidth() const { return m_Size.x; }
+  float GetHeight() const { return m_Size.y; }
+  const glm::vec2& GetOrigin() const { return m_Origin; }
 private:
   glm::mat4 m_ViewMatrix, m_ProjectionMatrix, m_VPMatrix;
   Transform2D m_Transform2D;
+  glm::vec2 m_Size;
+  glm::vec2 m_Origin;
 };
