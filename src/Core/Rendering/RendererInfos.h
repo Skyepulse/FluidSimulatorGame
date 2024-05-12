7c3cca2e35d1a74a89409b276f63ccae91f7916a
arthur <arthur.wuhrlin@gmail.com>
2024-05-12 15:54:09 +0200

    RendererInfos commentaries

--- src/Core/Rendering/RendererInfos.h
+++ src/Core/Rendering/RendererInfos.h
@@ -20,7 +20,8 @@ class TextureRenderProperty : public RenderProperty
 {
 public:
   /// @brief Create a Texture render properties that will be upload to the shader by calling Setproperties.
-  /// Warning : the data is a pointer, meaning that it must not been freed before using it in SetProperties
+  /// @attention The data is a pointer, meaning that it must not been freed before using it in SetProperties
+  /// @attention Data freeing is not handle by the Render property
   /// @param texture 
   TextureRenderProperty(const Texture* texture) : m_Texture(texture) {}
   virtual void SetProperties(std::shared_ptr<Shader> shader) const override
@@ -33,21 +34,26 @@ public:
     shader->SetInt("u_Texture", 0); // => TODO change for multiple texture binding support
     // ---------------------------- // 
   }
-protected:
+private:
   const Texture* const m_Texture;
 };
 
 class ColorRenderProperty : public RenderProperty
 {
 public:
+  /// @brief Create a Color render properties that will be upload to the shader by calling Setproperties.
+  /// @attention The data is a pointer, meaning that it must not been freed before using it in SetProperties
+  /// @attention Data freeing is not handle by the Render property
+  /// @param color 
   ColorRenderProperty(const glm::vec3* color) : m_Color(color) {}
   virtual void SetProperties(std::shared_ptr<Shader> shader) const override
   {
+    // Assert that the data used is not null
     CORE_ASSERT(m_Color, "The glm::vec3 pointer in the RenderProperty is null, you must not free it while having a RenderProperty holding a pointer to it");
     
     shader->SetFloat3("u_Color", *m_Color);
   }
-protected:
+private:
   const glm::vec3* const m_Color;
 };
 
