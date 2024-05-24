a543a4d28a876446f9f6561840540ebb47f99f4f
arthur <arthur.wuhrlin@gmail.com>
2024-05-24 20:05:39 +0200

    Window Resized event

---
+++ src/Core/Event/ApplicationEvent.h
@@ -0,0 +1,190 @@
+#pragma once
+
+#include "Event.h"
+
+#include <glm/glm.hpp>
+
+#include <sstream>
+#include <string>
+
+// -------------------------------------------------------------------------
+//                                KEYBOARD EVENT
+// -------------------------------------------------------------------------
+
+class KeyEvent : public Event
+{
+public:
+  KeyEvent(int keyCode) : Event(), m_KeyCode(keyCode) {}
+  int GetKey() { return m_KeyCode; }
+
+  CORE_CREATE_EVENT_CATEGORY_METHODS(EventCategory::KeyboardEventCategory)
+  virtual std::string ToString() const override 
+  {
+    std::stringstream ss;
+    ss << "[KeyEvent] Key " << m_KeyCode;
+    return ss.str(); 
+  } 
+private:
+  int m_KeyCode;
+};
+
+class KeyPressedEvent : public KeyEvent
+{
+public:
+  KeyPressedEvent(int keyCode) : KeyEvent(keyCode) {}
+
+  CORE_CREATE_EVENT_TYPE_METHODS(EventType::KeyPressed)
+  virtual std::string ToString() const override 
+  {
+    std::stringstream ss;
+    ss << KeyEvent::ToString() << " pressed";
+    return ss.str(); 
+  } 
+};
+
+class KeyReleasedEvent : public KeyEvent
+{
+public:
+  KeyReleasedEvent(int keyCode) : KeyEvent(keyCode) {}
+
+  CORE_CREATE_EVENT_TYPE_METHODS(EventType::KeyReleased)
+  virtual std::string ToString() const override 
+  {
+    std::stringstream ss;
+    ss << KeyEvent::ToString() << " released";
+    return ss.str(); 
+  } 
+};
+
+class KeyTypedEvent : public KeyEvent
+{
+public:
+  KeyTypedEvent(int keyCode) : KeyEvent(keyCode) {}
+
+  CORE_CREATE_EVENT_TYPE_METHODS(EventType::KeyTyped)
+  virtual std::string ToString() const override 
+  {
+    std::stringstream ss;
+    ss << KeyEvent::ToString() << " typed";
+    return ss.str(); 
+  } 
+};
+
+// -------------------------------------------------------------------------
+//                                MOUSE EVENT
+// -------------------------------------------------------------------------
+
+class MouseEvent : public Event
+{
+public:
+  MouseEvent(double mouseX, double mouseY) : Event(), m_MousePosition(glm::vec2(mouseX, mouseY)) {}
+  double GetMouseX() { return m_MousePosition.x; }
+  double GetMouseY() { return m_MousePosition.y; }
+  glm::vec2 GetMousePosition() const { return m_MousePosition; }
+
+  CORE_CREATE_EVENT_CATEGORY_METHODS(EventCategory::MouseEventCategory)
+  virtual std::string ToString() const override 
+  {
+    std::stringstream ss;
+    ss << "[MouseEvent] Position : (" << m_MousePosition.x << ", " << m_MousePosition.y << ")";
+    return ss.str(); 
+  } 
+private:
+  glm::vec2 m_MousePosition;
+};
+
+class MouseMovedEvent : public MouseEvent
+{
+public:
+  MouseMovedEvent(double mouseX, double mouseY) : MouseEvent(mouseX, mouseY) {}
+  
+  CORE_CREATE_EVENT_TYPE_METHODS(EventType::MouseMoved)
+  virtual std::string ToString() const override 
+  {
+    std::stringstream ss;
+    ss << MouseEvent::ToString() << " Mouse moved";
+    return ss.str(); 
+  } 
+};
+
+class MouseInputEvent : public MouseEvent
+{
+public:
+  MouseInputEvent(double mouseX, double mouseY, int mouseButton) : MouseEvent(mouseX, mouseY) {}
+  int GetMouseButton() const { return m_MouseButton; }
+  
+  CORE_CREATE_EVENT_CATEGORY_METHODS(EventCategory::MouseInputEventCategory)
+  virtual std::string ToString() const override 
+  {
+    std::stringstream ss;
+    ss << MouseEvent::ToString() << "Mouse Input : {}" << m_MouseButton;
+    return ss.str(); 
+  } 
+private:
+  int m_MouseButton;
+};
+
+class MousePressedEvent : public MouseInputEvent
+{
+public:
+  MousePressedEvent(double mouseX, double mouseY, int mouseButton) : MouseInputEvent(mouseX, mouseY, mouseButton) {}
+  
+  CORE_CREATE_EVENT_TYPE_METHODS(EventType::MousePressed)
+  virtual std::string ToString() const override 
+  {
+    std::stringstream ss;
+    ss << MouseEvent::ToString() << " pressed";
+    return ss.str(); 
+  } 
+};
+
+class MouseReleasedEvent : public MouseInputEvent
+{
+public:
+  MouseReleasedEvent(double mouseX, double mouseY, int mouseButton) : MouseInputEvent(mouseX, mouseY, mouseButton) {}
+  
+  CORE_CREATE_EVENT_TYPE_METHODS(EventType::MouseReleased)
+  virtual std::string ToString() const override 
+  {
+    std::stringstream ss;
+    ss << MouseEvent::ToString() << " released";
+    return ss.str(); 
+  } 
+};
+
+class MouseTypedEvent : public MouseInputEvent
+{
+public:
+  MouseTypedEvent(double mouseX, double mouseY, int mouseButton) : MouseInputEvent(mouseX, mouseY, mouseButton) {}
+  
+  CORE_CREATE_EVENT_TYPE_METHODS(EventType::MouseTyped)
+  virtual std::string ToString() const override 
+  {
+    std::stringstream ss;
+    ss << MouseEvent::ToString() << " typed";
+    return ss.str(); 
+  } 
+};
+
+// -------------------------------------------------------------------------
+//                                WINDOW EVENTS
+// -------------------------------------------------------------------------
+
+class WindowResizedEvent : public Event
+{
+public:
+  WindowResizedEvent(int width, int height) : Event(), m_Size(glm::ivec2(width, height)) {}
+  const glm::ivec2& GetSize() { return m_Size; } 
+
+  CORE_CREATE_EVENT_CATEGORY_METHODS(EventCategory::WindowEventCategory)
+  CORE_CREATE_EVENT_TYPE_METHODS(EventType::WindowResized)
+  virtual std::string ToString() const override 
+  {
+    std::stringstream ss;
+    ss << "[WindowResizedEvent] Size : " << m_Size.x << ", " << m_Size.y;
+    return ss.str(); 
+  } 
+
+private:
+  glm::ivec2 m_Size;
+};
---
+++ src/Core/Event/Event.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <memory>
+#include "../Log.h"
+
+#define CORE_BIND_EVENT_METHOD(className, eventMethodName) std::bind(&className::eventMethodName, this, std::placeholders::_1)
+
+#define CORE_CREATE_EVENT_CATEGORY_METHODS(eventCategory)                                                         \
+  virtual EventCategory GetEventCategory() const override { return eventCategory; }                               \
+  virtual bool IsEventCategory(EventCategory category) const override { return eventCategory == category; }       \
+
+#define CORE_CREATE_EVENT_TYPE_METHODS(eventType)                                                                 \
+  static EventType GetStaticType() { return eventType; }                                                    \
+  virtual EventType GetEventType() const override { return GetStaticType(); }                                     \
+  virtual bool IsEventType(EventType type) const override { return type == eventType; }   \
+  virtual const char* GetName() const { return #eventType; }
+
+/// @brief  TODO : CHANGE CATEGORIES TO FLAGS
+enum class EventCategory
+{
+  None = 0, WindowEventCategory, KeyboardEventCategory, MouseEventCategory, MouseInputEventCategory, MouseMovementEventCategory,
+};
+
+enum class EventType
+{
+  None = 0     ,
+  WindowResized, WindowClosed ,
+  KeyInputEvent, KeyPressed   , KeyReleased  , KeyTyped  ,
+  MousePressed , MouseReleased, MouseTyped, 
+  MouseMoved   ,  
+};
+
+inline EventType operator|(EventType a, EventType b)
+{
+  return static_cast<EventType>(static_cast<int>(a) | static_cast<int>(b));
+}
+
+class Event
+{
+public:
+  Event() : Handled(false) {}
+
+  virtual EventCategory GetEventCategory() const = 0;                           
+  virtual bool IsEventCategory(EventCategory category) const = 0; 
+                                                                                                              
+  virtual EventType GetEventType() const  = 0;                         
+  virtual bool IsEventType(EventType type) const = 0; 
+
+  virtual const char* GetName() const = 0;
+  virtual std::string ToString() const = 0;
+
+public:
+  bool Handled;
+};
+
+class EventDispatcher
+{
+  template <typename T>
+  using EventMethod = std::function<bool(T&)>;
+public:
+  EventDispatcher(Event& event) : m_Event(event) {}
+
+  template <typename T>
+  bool Dispatch(EventMethod<T> eventMethod)
+  {
+    if(m_Event.IsEventType(T::GetStaticType()))
+    {
+      m_Event.Handled = eventMethod(dynamic_cast<T&>(m_Event));
+      return true;
+    }
+
+    return false;
+  }
+
+private:
+  Event& m_Event;
+};
