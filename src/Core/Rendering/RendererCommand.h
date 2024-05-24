e1728b5ba4684ea448f090ba4103b6ed36a229fc
arthur <arthur.wuhrlin@gmail.com>
2024-05-24 20:06:17 +0200

    Set viewport size command

--- src/Core/Rendering/RendererCommand.h
+++ src/Core/Rendering/RendererCommand.h
@@ -10,5 +10,5 @@ public:
   static void Clear() { glClear(GL_COLOR_BUFFER_BIT); }
   static void ClearColor(float r = 0.3f, float g = 0.4f, float b = 0.4f, float a = 1.0f) { glClearColor(r, g, b, a); }
   static void EnableBlending(bool enabled) { enabled ? glEnable(GL_BLEND) : glDisable(GL_BLEND); }
-  static void SetPointSize(float size) { glPointSize(size); }
+  static void SetViewportSize(const glm::ivec2& size) { glViewport(0, 0, size.x, size.y); }
 };
