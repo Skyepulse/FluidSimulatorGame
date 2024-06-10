be95a5ad6e2a3af61317e14807ea8b5403dc1f28
Jean-Loup <jlarette-22@telecom-paris.fr>
2024-06-10 15:42:27 +0200

    fix release mode

--- src/Core/Core.h
+++ src/Core/Core.h
@@ -1,8 +1,8 @@
 #include "Log.h"
 
 #ifdef NDEBUG
-  #define CORE_ASSERT
-  #define ASSERT
+  #define CORE_ASSERT(PREDICATE, ...) ;
+  #define ASSERT(PREDICATE, ...) ;
 #else
   #define CORE_ASSERT(PREDICATE, ...) if(!(PREDICATE)) { CORE_CRITICAL("Assertion not verified in {} at line {}", __FILE__, __LINE__); CORE_CRITICAL(__VA_ARGS__); std::abort(); }
   #define ASSERT(PREDICATE, ...) if(!(PREDICATE)) { CRITICAL("Assertion not verified in {} at line {}", __FILE__, __LINE__); CRITICAL(__VA_ARGS__); std::abort(); }
